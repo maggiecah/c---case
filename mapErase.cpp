d731aaf2af8925c17daf0d12c5fd37e2fade3552
maggiecah <churuimin425@163.com>
2014-03-06 18:22:13 +0800

    add the leetcode

--- mapErase.cpp
+++ mapErase.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main()
 {
     map <int,int> dictory;
-    for(int i =0;i<5;i++)
+    for(int i =1;i<6;i++)
         dictory.insert(make_pair(i,i));
     for (map<int,int>::iterator it = dictory.begin();it!=dictory.end();it++)
-        if(it->first==1)
-             dictory.erase(it--);
+        if(it->first==3){
+          // it--;
+            dictory.erase(it);
+          // it--;
+        }
         else
         cout<<" "<<it->second;
        
